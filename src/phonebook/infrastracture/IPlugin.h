7d7e06c1b173824eaf5c9da0f76af2504667ab9a
S <sill49@yahoo.com>
2018-09-25 12:08:53 +0300

    Updated the IPlugin interaface.

--- src/phonebook/infrastracture/IPlugin.h
+++ src/phonebook/infrastracture/IPlugin.h
@@ -1,20 +1,16 @@
 #pragma once
 #include <vector>
 
-enum class Dependency;
-
 class IPlugin
 {
 public:
 	virtual ~IPlugin() = default;
 
-	//virtual std::vector<IPlugin*> getDependencies() const = 0;
+	virtual std::vector<std::string> getDependencies() const = 0;
 
 	virtual void init(std::vector<IPlugin*> const& dependencies) = 0;
 
 	virtual void init() = 0;
 
 	virtual int run() = 0;
-
-	virtual void shutDown() = 0;
 };
